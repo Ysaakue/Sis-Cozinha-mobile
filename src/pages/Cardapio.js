dcf9e0d85fe4844ba510ef16780b43f28a8da192
IsaacGGeraldo <42001506+IsaacGGeraldo@users.noreply.github.com>
2019-08-18 20:43:38 -0300

    configurações iniciais

--- src/pages/Cardapio.js
+++ src/pages/Cardapio.js
@@ -1,18 +1,38 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import api from '../services/api';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
 
 export default function Cardapio() {
+    const [ pratos, setPratos ] = useState('');
+
+    useEffect(() => {
+        async function loadPratos() {
+            const response = await api.get('/menuWeek/date/19-08-2019', null);
+
+            console.log(response);
+
+            setPratos(response.data);
+        }
+
+        loadPratos();
+    }, []);
+
     return (
-        <View style={styles.header}>
-            <Text>
-                Cardapio
+        <SafeAreaView style={styles.header}>
+            <Text style={styles.headerText}>
+                Cardápio
             </Text>
-        </View>
+        </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
     header: {
-
+        height: 40,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    headerText: {
+        fontSize: 30,
     }
 });
